da70921dd869b4c26b5d6822aecc5785351c985b
Vasil Vanchuk <v.vanchuk@tut.by>
2015-12-13 21:17:38 +0300

    About flow

---
+++ topics/flow.js
@@ -0,0 +1,20 @@
+// What is the output?
+console.log(1);
+setTimeout(function() {
+  console.log(2);
+}, 1000);
+setTimeout(function() {
+  console.log(3);
+}, 0);
+console.log(4);
+
+// What is the output?
+function say(a) {
+  alert(a);
+}
+say(1);
+setTimeout(say(2), 5000);
+setTimeout(function() {
+  say(3);
+}, 1000);
+setTimeout(say, 2000, 4);
