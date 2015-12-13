057c06e4bd620bed491a98bb581bbae2a15d8132
Vasil Vanchuk <v.vanchuk@tut.by>
2015-12-13 21:20:35 +0300

    Hoisting vs types

---
+++ topics/hoisting-vs-types.js
@@ -0,0 +1,82 @@
+// What is the output?
+var a = 1;
+function b() {
+  a = 10;
+  return;
+  function a() {
+  }
+}
+b();
+console.log(a);
+
+// What is the output?
+var a = {};
+var b = { key: 'b' };
+var c = { key: 'c' };
+
+a[b] = 123;
+a[c] = 456;
+console.log(a[b]);
+
+// What is the output?
+console.log("1" + 2);
+console.log(2 + "1");
+console.log(1 + 2 + 3 + 4 + "5");
+
+// What for use next contruction?
+function greetAll() {
+  var args = Array.prototype.slice.call(arguments, 0);
+}
+
+// What is the output?
+function test() {
+  console.log(a);
+  console.log(foo());
+
+  var a = 1;
+
+  function foo() {
+    return 2;
+  }
+}
+test();
+
+// What is the output?
+(function() {
+  alert(inner);
+  inner();
+  function inner() {
+    alert('inner');
+  }
+})();
+
+// What is the output?
+(function {
+  alert(inner);
+  inner();
+  var inner = function() {
+    alert('inner');
+  }
+})();
+
+// What is the output?
+(function() {
+  f();
+  f = function() {
+    console.log(1);
+  }
+})();
+
+function f() {
+  console.log(2)
+}
+f();
+
+// What is the output?
+(function() {
+  var x = 1;
+
+  function x() {};
+
+  console.log(x);
+})();
