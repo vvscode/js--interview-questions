d5daae7af3347bc4370cad8678be4d6368aca71d
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-17 08:07:55 +0300

    Add task with spiral output

---
+++ topics/complex.js
@@ -0,0 +1,12 @@
+// Spiral output for array
+var m = [
+  [0,  1,  2,  3,  4],
+  [5,  6,  7,  8,  9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19]
+];
+
+var spiral = function (m) {
+ // Put your code here
+}; // Should be [0, 1, 2, 3, 4, 9, 14, 19, 18, 17, 16, 15, 10, 5, 6, 7, 8, 13, 12, 11]
+
