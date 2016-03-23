a8856f5f2dd97b2d2a2d352fd8c769ffb53774ea
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-03-23 20:55:16 +0300

    Add elevator task from minddojo

--- topics/complex.js
+++ topics/complex.js
@@ -29,3 +29,41 @@ semverSor(arr); // Like [ "0.12", "1.0.5", "1.2.3.4.5.6.7", "1.23.45", "1.4.50",
 // Like 'ololo'. 
 // Variant -- the same without spaces like 'olo lo'
 // Variant - the same in functional style
+
+// We have next stub
+// Fill blank methods to describe algorithms for servicing multistory building with your elevator
+// You have only low-leve intarface (HardwareElevator) with three states - stoppes, move up, move down
+// Every floor has two buttons up/down
+// Inside cabin there are only buttons with numbers
+DIRECTION_DOWN = -1
+DIRECTION_NONE = 0
+DIRECTION_UP = 1
+
+function HardwareElevator(){};
+HardwareElevator.prototype = {
+    moveUp:function(){console.log('moving up');},
+    moveDown:function(){console.log('moving down');},
+    stopAndOpenDoors:function(){console.log('stopping and opening doors');},
+    getCurrentFloor:function(){console.log('getting current floor');},
+    getCurrentDirection:function(){console.log('getting current drection');}
+}
+
+function Elevator() {
+    this.hw = new HardwareElevator();
+    this.hw.addEventListener("doorsClosed", _bind(this.onDoorsClosed, this));
+    this.hw.addEventListener("beforeFloor", _bind(this.onBeforeFloor, this));
+}
+Elevator.prototype = {
+    onDoorsClosed: function(floor) {
+      // put your code here
+    },
+    onBeforeFloor: function(floor, direction) {
+      // put your code here
+    },
+    floorButtonPressed: function(floor, direction) {
+      // put your code here
+    },
+    cabinButtonPressed: function(floor) {
+      // put your code here
+    }
+}
