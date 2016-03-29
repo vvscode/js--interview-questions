60aef198d141b53968f86c247d9f3dc49ae6bfff
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-03-29 14:05:13 +0300

    about flow control from andrew

--- topics/flow.js
+++ topics/flow.js
@@ -25,3 +25,10 @@ $.ajax(url, function() { done = true });
 while(!done) {
   someWaitingStuff();
 }
+
+// Next code print '0,1,2,3,4,4,3,2,1,0'
+// Modify only functions ( not loops ) to get output as '4,3,2,1,0,0,1,2,3,4'
+var a = function(i) { console.log(i); };
+var b = function(i) { console.log(i); };
+for(var i = 0; i < 5; i++) {  a(i); }
+for(var i = 4; i>=0; i--) { b(i); }
