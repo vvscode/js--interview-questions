7582da2788acab4c3c6fcdda5459c12d0bc71dc8
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-05-17 16:12:32 +0300

    Complex task from codewars about calculations

--- topics/complex.js
+++ topics/complex.js
@@ -72,3 +72,14 @@ Elevator.prototype = {
 // Like: Chain([link1,link2,link3]).mapAsync(link => get(link)).mampAsync(page => printPage)
 
 // Write code which will output itself into console/document
+
+//We want to write calculations using functions and get the results. Let's have a look at some examples:
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+//Requirements:
+//There must be a function for each number from 0 ("zero") to 9 ("nine")
+//There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy (divided_by in Ruby)
+//Each calculation consist of exactly one operation and two numbers
+//The most outer function represents the left operand, the most inner function represents the right operand
