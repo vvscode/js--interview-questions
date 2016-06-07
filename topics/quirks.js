b4e43a480ed1c0e39fe91b136dbda202f026e1b8
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-06-07 21:17:29 +0300

    Add some tricky questions from diff companies

--- topics/quirks.js
+++ topics/quirks.js
@@ -18,3 +18,25 @@ a // ?
 // What will be the output
 function foo(){console.log("hello")}
 foo.call.call.call.apply(function bar(x) {console.log(x)}, [this,"world"])
+
+// What will be the output
+console.log("1" + 2);
+console.log(2 + "1");
+console.log(1 + 2 + 3 + 4 + "5"); //<-- nb
+
+// Make it true
+var a;
+/* put your code here */
+a !== a; // should be true
+
+// What will be the output
+var scores = [98, 74,85, 77, 93,100,89];
+var total = 0;
+for (var score in scores) { 
+  total += score;
+} 
+var mean = total / scores.length;
+console.log(mean); //?
+
+// What will be the output
+[2, 3, -1, -6, 0, -108, 42, 10].sort();
