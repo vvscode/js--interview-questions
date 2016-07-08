e7a6f3e973653bf5147f8708f5edf981f19c9b74
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-07-08 09:44:52 +0300

    All wins game

--- topics/complex.js
+++ topics/complex.js
@@ -117,3 +117,18 @@ function logIt(){
     var text = 'inside';
 };
 logIt();
+
+// Game where everyone win. Output?
+<button id="btn-0">Button 1!</button>
+<button id="btn-1">Button 2!</button>
+<button id="btn-2">Button 3!</button>
+<script type="text/javascript">
+    var prizes = ['A Unicorn!', 'A Hug!', 'Fresh Laundry!'];
+    for (var btnNum = 0; btnNum < prizes.length; btnNum++) {
+        // for each of our buttons, when the user clicks it...
+        document.getElementById('btn-' + btnNum).onclick = function() {
+            // tell her what she's won!
+            alert(prizes[btnNum]);
+        };
+    }
+</script>
