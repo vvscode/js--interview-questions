6b73ce4ae48ca899724b31578bff4909bb268abd
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-07-31 15:45:38 +0300

    Update quirks.js

--- topics/quirks.js
+++ topics/quirks.js
@@ -85,3 +85,18 @@ alert(typeof bar());
 
 // Output?
 console.log("1" - - "1")
+
+// what is the output?
+var x = 3;
+var foo = {
+    x: 2,
+    baz: {
+        x: 1,
+        bar: function() {
+            return this.x;
+        }
+    }
+}
+var go = foo.baz.bar;
+alert(go());
+alert(foo.baz.bar());
