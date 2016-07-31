de4e13a256a7fbf7040e42378debee751efca88a
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-07-31 15:49:28 +0300

    Update quirks.js

--- topics/quirks.js
+++ topics/quirks.js
@@ -106,3 +106,13 @@ var myArr = ['foo', 'bar', 'baz'];
 myArr.length = 0;
 myArr.push('bin');
 console.log(myArr);
+
+// Output?
+var x = 0;
+function foo() {
+    x++;
+    this.x = x;
+    return foo;
+}
+var bar = new new foo;
+console.log(bar.x);
