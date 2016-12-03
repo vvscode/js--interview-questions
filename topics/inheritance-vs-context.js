8516e68f0d157920cfe2ec80a8afee28d9a956a6
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-12-03 06:11:29 +0300

    From Sinesis
    First met at https://www.sitepoint.com/5-typical-javascript-interview-exercises/  but last week I met it in real interview

--- topics/inheritance-vs-context.js
+++ topics/inheritance-vs-context.js
@@ -157,3 +157,9 @@ function f(x, y) {
   );
 }
 f();
+
+//  Create “native” methods
+// Define a repeatify function on the String object. 
+// The function accepts an integer that specifies how many times the string has to be repeated. 
+// The function returns the string repeated the number of times specified. For example:
+console.log('hello'.repeatify(3)); // hellohellohello
