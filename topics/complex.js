2d767050ee522355f13d7540db60699458acdd27
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-01-24 23:00:48 +0300

    Update complex.js

--- topics/complex.js
+++ topics/complex.js
@@ -229,3 +229,10 @@ doSomething().then(function () {
 doSomething().then(doSomethingElse());
 
 doSomething().then(doSomethingElse);
+
+// Implement function to remove duplication from list (array)
+
+// You have a function rand7() that generates a random integer from 1 to 7. 
+// Use it to write a function rand5() that generates a random integer from 1 to 5.
+// rand7() returns each integer with equal probability. 
+// rand5() must also return each integer with equal probability.
