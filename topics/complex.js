9e7a3536753e37c3b1de061aa56f73255c4a6836
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-10-17 23:45:27 +0300

    Legendary FizzBuzz

--- topics/complex.js
+++ topics/complex.js
@@ -287,3 +287,11 @@ doSomething().then(doSomethingElse);
 
 // implement shuffle(list) function which suffle list ( randomize list elements )
 
+/*
+Legendary FizzBuzz
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”.
+
+Additional restriction (optional) - you can't use if/switch/?: statements
+*/
